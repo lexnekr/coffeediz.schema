d2bc9a715f66b15a7c2a920a153cce3ff3291f16
Aleksey Zadoiniy <a@coffeediz.ru>
2016-04-11 14:00:28 +0400

    v.1.0.21 Исправлена ошибка Канонического URL для компонента Article

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?
 $arModuleVersion = array(
-    "VERSION" => "1.0.20",
-    "VERSION_DATE" => "2016-02-36 15:00:00"
+    "VERSION" => "1.0.21",
+    "VERSION_DATE" => "2016-04-11 14:00:00"
 );
 ?>
